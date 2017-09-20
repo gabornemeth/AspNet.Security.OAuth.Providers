b3cc2a1c66d71769ff31626456f6e6c7b19ad0ee
Jack Thorpe <jackthorpe1@hotmail.com>
2017-09-20 20:54:51 +0100

    removed commented out StravaAuthenticationHandler.CreateTicketAsync

--- src/AspNet.Security.OAuth.Strava/StravaAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Strava/StravaAuthenticationHandler.cs
@@ -27,37 +27,6 @@ namespace AspNet.Security.OAuth.Strava
             [NotNull] ILoggerFactory factory,
             [NotNull] UrlEncoder encoder,
             [NotNull] ISystemClock clock
-            ) : base(options, factory, encoder, clock)
-        {
-        }
-        //protected override async Task<AuthenticationTicket> CreateTicketAsync(
-        //    [NotNull] ClaimsIdentity identity,
-        //    [NotNull] AuthenticationProperties properties,
-        //    [NotNull] OAuthTokenResponse tokens)
-        //{
-        //    var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
-        //    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
-
-        //    var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        Logger.LogError("An error occurred while retrieving the user profile: the remote server " +
-        //                        "returned a {Status} response with the following payload: {Headers} {Body}.",
-        //                        /* Status: */ response.StatusCode,
-        //                        /* Headers: */ response.Headers.ToString(),
-        //                        /* Body: */ await response.Content.ReadAsStringAsync());
-
-        //        throw new HttpRequestException("An error occurred while retrieving the user profile.");
-        //    }
-
-        //    var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-        //    var principal = new ClaimsPrincipal(identity);
-        //    var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload);
-        //    await Options.Events.CreatingTicket(context);
-
-        //    return new AuthenticationTicket(principal, properties, Scheme.Name);
-        //}
+            ) : base(options, factory, encoder, clock) { }
     }
 }
